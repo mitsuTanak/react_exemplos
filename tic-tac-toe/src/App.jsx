7a7f38a305e9732995d44e06597a2db370fb950a
Mitsuaki Tanaka <130664643+mitsuTanak@users.noreply.github.com>
2024-03-08 10:51:52 -0300

    dark mode

--- tic-tac-toe/src/App.jsx
+++ tic-tac-toe/src/App.jsx
@@ -3,10 +3,14 @@ function App() {
   return (
     <div className='App'>
       <h1>Jogo da Velha</h1>
-      
+
       <Board />
+
+      <button onclick="">Toggle dark mode</button>
+
     </div>
   );
 }
+
 
 export default App;
