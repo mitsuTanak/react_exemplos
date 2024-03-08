2aa49ee8474783586fdb89a8647358d9f3bf52c8
Mitsuaki Tanaka <130664643+mitsuTanak@users.noreply.github.com>
2024-03-08 12:39:32 -0300

    a

--- tic-tac-toe/src/App.jsx
+++ tic-tac-toe/src/App.jsx
@@ -3,10 +3,7 @@ function App() {
   return (
     <div className='App'>
       <h1>Jogo da Velha</h1>
-
       <Board />
-
-      <button onclick="">Toggle dark mode</button>
 
     </div>
   );
---
+++ tic-tac-toe/src/components/Toggle.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function dark(handleChange, isChecked) {
+    return(
+        <div className="dark-container" typeof="checkbox" ></div>
+    )
+}
