b5f18db00d5d0fc3ed87ed74df70231433c45959
Mitsuaki Tanaka <130664643+mitsuTanak@users.noreply.github.com>
2024-03-21 14:20:15 -0300

    debug + comentarios

--- formulario-react_alunos/src/components/forms/form.js
+++ formulario-react_alunos/src/components/forms/form.js
@@ -1,140 +1,134 @@
-// Imposrta o React e os hoocks necessarios do React
 import React, {useState} from 'react';
-// 
-// 
 import {useForm} from 'react-hook-form';
-// 
 import '../css/style.css';
-// 
-import minhaImagem from '../images/login.png';
-// 
+import minhaImagem from '../images/login.jpg';
 import verified from '../images/verified.png';
 
-// 
+//Função principal que representa o componetne de fornulário
 function MyForm() {
-    // 
-    // 
     const { register, handleSubmit, formState: {errors}} = useForm();
-    // 
-    const [status, setStatus] = useState ({SubmittedSuccessfully: false, loading: false, showPassword: false});
+    //Para controlar o estado do formulário
+    const [status, setStatus] = useState({submittedSuccessfully: false, loading: false, showPassword: false});
 
-    // 
+    //Função que é chamada quando o formulario é enviado
     const onSubmit = data => {
-        // 
-        setStatus({...status, loading: true}); //
+        ({...status, loading: true});//Atualiza o estado para indicar que está carregando
 
-        // 
+
+        //Simula um tempo de espera antes de atualizar o estado para sucesso
         setTimeout(() => {
-            setStatus({SubmittedSuccessfully: true, loading: false});
+            setStatus({ submittedSuccessfully: true, loading: false});
         }, 1000);
     };
 
-    const generateErrorMensage = fieldName =>{
-        return{
-            required: `${fieldName} é obrigatorio`,//
-            pattern: fieldName === 'email' ? 'Formato de email invalido': null//
-        }
+    //Função para gerar mensagens de erro com base no nome do campo
+    const generateErrorMessage = fielName => {
+        return {
+            required: `${fielName} é obrigatório`, //Mensagem para campos obrigatórios
+            pattern: fielName === 'email'? 'Formato de e-mail inválido': null //Mensagem para formulário de e-mail inválido 
+        };
     };
 
-    // 
-    const hadleReturnButtonClick = () => {
-        // 
+    //Função para lidar com o retorno à página inicial
+    const handleReturnButtonClick = () => {
+        //Redireciona para a página inicial
         window.location.href = '/';
     };
 
-    // 
-    return(
+    //Renderização condicional (usando ternário ?) com base no sucesso de envio do formulário
+    return (
         <div className='container'>
-            {status.SubmittedSuccessfully ? (
-                // 
-                <div className='success-message'>
+            {status.submittedSuccessfully ? (
+                 <div className='success-message'>
                     <img src={verified} alt='Verificado' />
-                    <h2>Formulario Enviado com Sucesso</h2>
-                    <p>Obrigado por enviar o formulario.</p>
-                    <button className='btn' onClick={hadleReturnButtonClick}>Retornar</button>
-                </div>
-            ): (
-                // 
-                <div className='form-sing-up'>
+                    <h2>Formulário Enviado com Sucesso!</h2>
+                    <p>Obrigado por enviar o formulário.</p>
+                    <button className='btn' onClick={handleReturnButtonClick}>Retornar</button>
+                 </div>
+            ):(
+                //Se o formulário ainda não foi enviado co sucesso, exibe o formulário para preenchimento
+                <div className='form-sign-up'>
                     <div>
-                        <img src={minhaImagem} alt="imagem de uma pessoa se cadastrando pelo celular" />
+                        <img src={minhaImagem} alt='imagem de uma pessoa se cadastrando pelo celular' />
                     </div>
 
                     <section>
                         <h1>Inscreva-se</h1>
 
-                        {/*  */}
+                        {/* Formulário com campo controlado pelo hook useForm*/}
                         <form onSubmit={handleSubmit(onSubmit)}>
-
-                            {/*  */}
+                        
+                        {/* Campo nome*/}
                             <div className='form flex'>
-                                <label htmlFor="name">Nome</label>
-                                <input
-                                    {...register("firstName", generateErrorMensage("Nome"))}
-                                    placeholder='Nome'
-                                    id='name'
-                                    autoComplete='off'
-                                    className={errors.fieldName ? 'error' : ''}
+                                <label htmlFor='name'>Nome</label>
+                                <input 
+                                {...register("firstName", generateErrorMessage("Nome"))}
+                                placeholder="Nome"
+                                id='name'
+                                autoComplete='off'
+                                className={errors.firstName ? 'error' : ''}
                                 />
                                 {errors.firstName && <span className='error-message'>{errors.firstName.message}</span>}
                             </div>
 
-                            {/*  */}
-                            <div className='form flex'>
-                                <label htmlFor="sobrenome">Sobrenome</label>
-                                <input
-                                    {...register("lastName", generateErrorMensage("Sobrenome"))}
-                                    placeholder='Sobrenome'
-                                    id='sobrenome'
-                                    autoComplete='off'
-                                    className={errors.lastName ? 'error' : ''}
+
+                             {/* Campo sobrenome*/}
+                             <div className='form flex'>
+                                <label htmlFor='sobrenome'>Sobrenome</label>
+                                <input 
+                                {...register("lastName", generateErrorMessage("Sobrenome"))}
+                                placeholder="Sobrenome"
+                                id='sobrenome'
+                                autoComplete='off'
+                                className={errors.lastName ? 'error' : ''}
                                 />
                                 {errors.lastName && <span className='error-message'>{errors.lastName.message}</span>}
                             </div>
 
-                            {/*  */}
-                            <div className='form flex'>
-                                <label htmlFor="email">E-mail</label>
-                                <input
-                                    {...register("email", generateErrorMensage("E-mail"))}
-                                    placeholder='E-mail'
-                                    id='email'
-                                    autoComplete='off'
-                                    className={errors.email ? 'error' : ''}
+
+                             {/* Campo E-mail*/}
+                             <div className='form flex'>
+                                <label htmlFor='email'>Email</label>
+                                <input 
+                                {...register("email", generateErrorMessage("E-mail"))}
+                                placeholder="E-mail"
+                                id='email'
+                                autoComplete='off'
+                                className={errors.firstName ? 'error' : ''}
                                 />
                                 {errors.email && <span className='error-message'>{errors.email.message}</span>}
                             </div>
 
-                            {/*  */}
-                            <div className='form flex'>
-                                <label htmlFor="password">Senha</label>
-                                <div className='password-input-container'>
-                                    <input
-                                        {...register("password", generateErrorMensage("Senha"))}
-                                        placeholder='Senha'
-                                        id='password'
-                                        autoComplete='off'
-                                        className={errors.password ? 'error' : ''}
-                                    />
+                             {/* Campo senha*/}
+                             <div className='form flex'>
+                                <label htmlFor='password'>Senha</label>
+                                <div className="password-input-container">
+                                <input 
+                                {...register("password", generateErrorMessage("Senha"))}
+                                placeholder="Senha"
+                                id='password'
+                                autoComplete='off'
+                                type={status.showPassword ? 'text' : 'password'}
+                                className={errors.password ? 'error' : ''}
+                                />
 
-                                    <button
-                                        type='burron' //
-                                        className='password-toggle'//
-                                        onClick={() => setStatus({...status, showPassword: !status,showPassword})}
-                                        // 
-                                    >
-                                        {status.showPassword ? <i className='fa fa-eye'></i> : <i className='fa fa-eye-slash'></i>}
-                                        {/*  */}
-                                    </button>
+                                <button 
+                                type="button"
+                                className='password-toggle'
+                                onClick={() => setStatus({...status, showPassword: !status.showPassword})}
+                                >
+
+                                    {status.showPassword ? <i className="fas fa-eye"></i> : <i className='fas fa-eye-slash'></i>}
+                                    
+                                </button>
                                 </div>
+                               {errors.password && <span className='error-message'>{errors.password.message}</span>}
+                             </div>
 
-                                {errors.password && <span className='error-message'>{errors.password.message}</span>}
-                                
-                            </div>
-                            {/*  */}
-                            <button className='btn' type='submit' disabled={status.loading}>
-                            
-                            </button>
+                             {/* Botãp Enviar */}
+                             <button className='btn' type="submit" disabled={status.loading}>
+                                {status.loading ? 'Enviando...': 'Enviar'}
+                             </button>
                         </form>
                         {status.loading && <p>Carregando...</p>}
                     </section>
@@ -143,6 +137,8 @@ function MyForm() {
         </div>
     );
 
-};
 
+}
+
+//Exporta componente "MyForm"
 export default MyForm;
